33202ef381a1af43f0966d84f3e90c4b229be4e4
xsebov01 <sebovavladimira@gmail.com>
2021-03-30 19:21:58 +0200

    done

---
+++ Labs/07-ffs/07-ffs/07-ffs.srcs/sim_1/new/tb_d_ff_rst.vhd
@@ -0,0 +1,139 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 30.03.2021 15:43:04
+-- Design Name: 
+-- Module Name: tb_d_ff_rst - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_d_ff_rst is
+--  Port ( );
+end tb_d_ff_rst;
+
+architecture Behavioral of tb_d_ff_rst is
+        constant c_CLK_100MHZ_PERIOD    : time  := 10ns;
+        
+        signal s_clk_100MHz     : std_logic; 
+        signal s_rst            : std_logic;
+        signal s_d              : std_logic;
+        signal s_q              : std_logic;
+        signal s_q_bar          : std_logic;
+begin
+        uut_d_ff_rst: entity work.d_ff_rst
+        port map(
+                clk     =>  s_clk_100MHz,
+                rst     =>  s_rst,
+                d       =>  s_d,
+                q       =>  s_q,
+                q_bar   =>  s_q_bar
+                );
+
+    --------------------------------------------------------------------
+    -- Clock generation process
+    --------------------------------------------------------------------
+    p_clk_gen : process
+    begin
+        while now < 40 ms loop        
+            s_clk_100MHz <= '0';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+            s_clk_100MHz <= '1';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+        end loop;
+        wait;
+    end process p_clk_gen;
+    
+    --------------------------------------------------------------------
+    -- Reset generation process
+    --------------------------------------------------------------------
+     p_reset_gen : process
+        begin
+            s_rst <= '0';
+            wait for 28 ns;
+            
+            -- Reset activated
+            s_rst <= '1';
+            wait for 13 ns;
+    
+            --Reset deactivated
+            s_rst <= '0';
+            
+            wait for 17 ns;
+            
+            s_rst <= '1';
+            wait for 33 ns;
+            
+            wait for 660 ns;
+            s_rst <= '1';
+    
+            wait;
+     end process p_reset_gen;
+
+    --------------------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------------------
+    p_stimulus : process
+    begin
+        report "Stimulus process started" severity note;
+        
+        s_d  <= '0';
+        wait for 14 ns;
+        
+        s_d  <= '1';
+        wait for 2 ns;
+        
+        wait for 8 ns;
+        s_d  <= '0';
+        wait for 6 ns;
+        
+        wait for 4 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+   
+        s_d  <= '0';
+        
+        wait for 14 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+       
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+end Behavioral;
---
+++ Labs/07-ffs/07-ffs/07-ffs.srcs/sim_1/new/tb_d_latch.vhd
@@ -0,0 +1,153 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 28.03.2021 17:52:57
+-- Design Name: 
+-- Module Name: tb_d_latch - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_d_latch is
+--  Port ( );
+end tb_d_latch;
+
+architecture Behavioral of tb_d_latch is
+        signal s_en       : std_logic; 
+        signal s_arst     : std_logic;
+        signal s_d        : std_logic;
+        signal s_q        : std_logic;
+        signal s_q_bar    : std_logic;
+begin
+        uut_d_latch: entity work.d_latch
+        port map(
+                en      =>  s_en,
+                arst    =>  s_arst,
+                d       =>  s_d,
+                q       =>  s_q,
+                q_bar   =>  s_q_bar
+        );
+    --------------------------------------------------------------------
+    -- Reset generation process
+    --------------------------------------------------------------------
+     p_reset_gen : process
+     begin
+         s_arst <= '0';
+         wait for 38 ns;
+         
+         -- Reset activated
+         s_arst <= '1';
+         wait for 53 ns;
+    
+         --Reset deactivated
+         s_arst <= '0';
+        
+         wait for 80 ns;
+         s_arst <= '1';
+    
+         wait;
+     end process p_reset_gen;
+    
+    --------------------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------------------
+    p_stimulus : process
+    begin
+        report "Stimulus process started" severity note;
+        
+        s_d  <= '0';
+        s_en <= '0';
+        wait for 10 ns;
+        
+        --remember/hold values (no value to hold)
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        --reset set to 1 -> all values '0' except q_bar
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+    
+        --Reseting output q
+        s_d  <= '0';
+        s_en <= '1';
+        assert ((s_arst = '0') and (s_en = '1'))
+        report "s_en setted to one -> Reseting output q" severity note;	
+        wait for 10 ns;
+        
+        --Seting output q - en is setted to 1
+        s_d  <= '1';
+        assert ((s_arst = '0') and (s_en = '1'))
+        report "s_en setted to one -> Seting output q" severity note;	
+        wait for 10 ns;
+        
+        
+        s_d  <= '0';
+        wait for 10 ns;   
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        --reset set to 0 - again operating 
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+    
+    
+        -- Remember/hold values 
+        s_en <= '0';
+        assert ((s_arst = '0') and (s_en = '0'))
+        report "s_en setted to zero -> remember/hold value" severity note;
+        wait for 10 ns;
+        
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        --reset set to 1 - all values should be '0'
+        wait for 10 ns;
+        s_d  <= '0';
+        wait for 10 ns;
+        s_d  <= '1';
+        wait for 10 ns;
+        s_d  <= '0';
+        
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+end Behavioral;
